a7dcfeed91727968eaa12a573064986e22f1ec43
akina <1170689710@qq.com>
2024-11-19 19:39:49 +0800

    基本完成007,但listbox缺少scroll

--- 007.py
+++ 007.py
@@ -1,4 +1,9 @@
 import tkinter
+from tkinter import messagebox
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 
 class Fib:
@@ -42,8 +47,10 @@ class Frame:
         self.config = {
             "geometry": "800x600+200+200",
             "font": ("Arial", 12),
-            "title": "DNA突变点查找",
+            "title": "兔子问题和递推",
             "label_n": "请输入兔子要繁殖多少代:",
+            "label_k": "请输入兔子要繁殖多少对:",
+            "button": "求解",
             "author": "Thank you for your use, author @ 孙伟嘉"
         }
         window = tkinter.Tk()
@@ -55,7 +62,29 @@ class Frame:
         label_n = self.__create_label__(window, text=self.config["label_n"],
                                         width=20, height=2)
         label_n.place(x=65, y=50)
-        self.__combo_box__(window, 20, 5, 265, 60, 1, 1000)
+        label_k = self.__create_label__(window, text=self.config["label_k"],
+                                        width=20, height=2)
+        label_k.place(x=365, y=50)
+        entry_n = self.__combo_box__(window, width=10, height=5, x=265, y=60, range_a=1, range_b=100)
+        entry_k = self.__combo_box__(window, width=10, height=5, x=565, y=60, range_a=1, range_b=100)
+
+        def f():
+            if not entry_n.get() or not entry_k.get():
+                messagebox.showwarning(title="警告", message="请先输入")
+                return
+            tup = (int(entry_n.get()), int(entry_k.get()))
+            fib = Fib(tup=tup, method=1)
+            fib_list = fib.get_feb_list
+            fib_num = fib.get_fib_num
+            messagebox.showinfo(title="求解结果", message=f"经过{entry_n.get()}代繁殖后,共有{fib_num}对兔子.")
+            d = Draw(window)
+            fib_index = d.create_index(fib_list)
+            d.draw_chart(x_data=fib_list, x_index=fib_index, pic_size={"x": 7, "y": 4}, title="柱形示意图",
+                         y_label="对数", x_label="代", location={"x": 50.0, "y": 180.0})
+            pass
+
+        button_calculate = self.__create_button__(window, width=5, height=1, button_click=f)
+        button_calculate.place(x=680, y=55)
 
         window.resizable(False, False)
         window.mainloop()
@@ -70,14 +99,19 @@ class Frame:
     def __create_entry__(self, master, width, var):
         return tkinter.Entry(master, font=self.config["font"], width=width, textvariable=var)
 
+    def __create_button__(self, master, width, height, button_click):
+        return tkinter.Button(master, text=self.config["button"], font=self.config["font"], width=width, height=height,
+                              command=button_click)
+
     def __combo_box__(self, master, width, height, x, y, range_a, range_b):
         var1 = tkinter.StringVar()
-        entry_n = self.__create_entry__(master, width=width, var=var1)
-        entry_n.place(x=x, y=y)
+        entry = self.__create_entry__(master, width=width, var=var1)
+        entry.place(x=x, y=y)
         listbox = tkinter.Listbox(master, width=width, height=height)
 
         def entry_n_bind(event):
-            listbox.place(x=x, y=y+12)
+            listbox.place(x=x, y=y + 20)
+            listbox.delete(0, "end")
             for i in range(range_a, range_b):
                 listbox.insert("end", str(i))
                 # scroll
@@ -88,8 +122,44 @@ class Frame:
             var1.set(listbox.get(index))
             listbox.place_forget()
 
-        entry_n.bind("<Button-1>", entry_n_bind)
+        entry.bind("<Button-1>", entry_n_bind)
         listbox.bind("<ButtonRelease-1>", listbox_curselection)
+        return entry
+
+
+class Draw:
+    def __init__(self, master):
+        self.__master = master
+
+    def draw_chart(self, x_data, x_index, pic_size: dict[float:float] = {"x": 7, "y": 3}, title="", x_label="x",
+                   y_label="y",
+                   location: dict[str:float] = {"x": 60.0, "y": 250.0}):
+        plt.rcParams["font.sans-serif"] = ["SimHei"]
+        plt.rcParams["axes.unicode_minus"] = False
+        data = x_data
+        index = x_index
+        fig = Figure(figsize=(pic_size["x"], pic_size["y"]), dpi=100)
+        ax = fig.add_subplot(111)
+        bars = ax.bar(index, data, color=['red'])
+        ax.set_ylim(0, max(data) + 1)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        ax.set_title(title)
+        for bar, height in zip(bars, data):
+            y_val = height
+            ax.text(bar.get_x() + bar.get_width() / 2 - 0.1, y_val,
+                    y_val, ha='center', va='bottom')
+        canvas = FigureCanvasTkAgg(fig, master=self.__master)
+        canvas.draw()
+        canvas.get_tk_widget().place(x=location["x"], y=location["y"])
+        pass
+
+    @staticmethod
+    def create_index(data_list: list):
+        index = []
+        for i in range(len(data_list)):
+            index.append(i + 1)
+        return index
 
 
 if __name__ == '__main__':
